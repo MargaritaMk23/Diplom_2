30c3abead01fc2181dd4c687a5717c4bb1d6932c
Margaritka Kostrova <margaritka100702@yandex.ru>
2026-06-11 23:46:59 +0700

    Final project

---
+++ src/main/java/constants/IngredientsData.java
@@ -0,0 +1,13 @@
+package constants;
+
+public class IngredientsData {
+
+    public static final String BUN =
+            "61c0c5a71d1f82001bdaaa6d";
+
+    public static final String SAUCE =
+            "61c0c5a71d1f82001bdaaa72";
+
+    public static final String WRONG_HASH =
+            "12345wrongHash";
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,34 @@
+package model;
+
+public class User {
+
+    private String email;
+    private String password;
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
